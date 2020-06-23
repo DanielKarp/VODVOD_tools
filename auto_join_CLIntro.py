4be6fea1a74675c4344ffd306886da77ea4ad3fc
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-06-23 11:31:59 -0700

    added basic arg-parse functionality

--- auto_join_CLIntro.py
+++ auto_join_CLIntro.py
@@ -1,27 +1,27 @@
+import argparse
 import os
-import sys
 
 CL_INTRO = 'CL-Intro.mp4'
 LISTFILE = 'concat_list.txt'
 
 def main():
     result = []
-    if len(sys.argv) > 1:
-        searchstr = sys.argv[1]
-        vids = [file for file in os.listdir(os.curdir) if searchstr in file]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_name', help='The [partial] file name of the video[s] you want to edit')
+    args = parser.parse_args()
 
-        for vid in vids:
-            with open(LISTFILE, mode='w') as f:
-                print('file', CL_INTRO, file=f)
-                print('file', f"'{vid}'", file=f)
-            output = f'"{os.path.splitext(vid)[0] + "_FINAL" + os.path.splitext(vid)[1]}"'
-            command = f'ffmpeg -f concat -safe 0 -i {LISTFILE} -c copy {output}'
-            print(f'\n****{command}\n')
-            result.append(output)
-            os.system(command)
-    else:
-        print(
-            'Enter a file name or substring of a file name in the current directory. All files that match will have CL-Intro prepended. _FINAL will be appended to the file name.')
+    vids = [file for file in os.listdir(os.curdir) if args.file_name in file]
+
+    for vid in vids:
+        with open(LISTFILE, mode='w') as f:
+            print('file', CL_INTRO, file=f)
+            print('file', f"'{vid}'", file=f)
+        output = f'"{os.path.splitext(vid)[0] + "_FINAL" + os.path.splitext(vid)[1]}"'
+        command = f'ffmpeg -f concat -safe 0 -i {LISTFILE} -c copy {output}'
+        print(f'\n****{command}\n')
+        result.append(output)
+        os.system(command)
+
     if os.path.exists(LISTFILE):
         os.remove(LISTFILE)
     print('Batch operation completed. Files created:')
