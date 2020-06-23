37f949904c647e5fc6e75efc470b5fd28123bec6
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-06-23 12:02:16 -0700

    added --no-summary

--- auto_join_CLIntro.py
+++ auto_join_CLIntro.py
@@ -8,6 +8,7 @@ def main():
     result = []
     parser = argparse.ArgumentParser()
     parser.add_argument('file_name', help='The [partial] file name of the video[s] you want to edit')
+    parser.add_argument('--no-summary', action='store_false', dest='summary', help="Don't print a summary after completion")
     args = parser.parse_args()
 
     vids = [file for file in os.listdir(os.curdir) if args.file_name in file]
@@ -24,6 +25,11 @@ def main():
 
     if os.path.exists(LISTFILE):
         os.remove(LISTFILE)
+    if args.summary:
+        print_summary(result)
+
+
+def print_summary(result):
     res_str = f'Batch operation completed. {len(result)} files created:'
     sep = '*' * len(res_str)
     print(sep)
