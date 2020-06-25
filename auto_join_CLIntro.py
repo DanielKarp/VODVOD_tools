8c7141706be113bdff9493cf8b601faaa39d7e66
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-06-24 19:50:40 -0700

    added verbose delete list, modified verbose list, made make command its own function, renamed init parser to setup parser

--- auto_join_CLIntro.py
+++ auto_join_CLIntro.py
@@ -8,27 +8,7 @@ LISTFILE = 'concat_list.txt'
 
 def main():
     result = []
-    parser = argparse.ArgumentParser(description='A tool to automate joining CL-Intro.mp4 to a clip or clips.',
-                                     epilog=f'Written by {__author__}')
-    parser.add_argument('-V', '--version',
-                        action='version',
-                        version=f"%(prog)s ({__version__})")
-    parser.add_argument('-v', '--verbose',
-                        action='store_true',
-                        dest='verbose',
-                        help='Enable verbose output')
-    parser.add_argument('file_name',
-                        help='The [partial] file name of the video[s] you want to edit')
-    parser.add_argument('-n', '--no-summary',
-                        action='store_false',
-                        dest='summary',
-                        help="Don't print a summary after completion")
-    parser.add_argument('-s', '--simulate',
-                        action='store_true',
-                        dest='simulate',
-                        help="Simulate calls to ffmpeg (useful for testing or seeing which files would be affected)")
-    args = parser.parse_args()
-
+    args = setup_parser()
     vids = [file for file in os.listdir(os.curdir) if args.file_name in file]
     if args.verbose:
         verbose_intro(vids)
@@ -36,23 +16,44 @@ def main():
         with open(LISTFILE, mode='w') as f:
             print('file', CL_INTRO, file=f)
             print('file', f"'{vid}'", file=f)
-        output = f'"{os.path.splitext(vid)[0] + "_FINAL" + os.path.splitext(vid)[1]}"'
-        command = f'ffmpeg -f concat -safe 0 -i {LISTFILE} -c copy {output}'
+        output, command = make_command(vid)
         if args.verbose:
             verbose_list(vid, command)
         result.append(output)
         if args.simulate:
-            print('- some ffmpeg output -\n' * 5)
+            if args.verbose:
+                print('- some ffmpeg output -\n' * 5)
         else:
             os.system(command)
     if os.path.exists(LISTFILE):
         os.remove(LISTFILE)
+        if args.verbose:
+            verbose_delete_list()
     if args.summary:
         print_summary(result, args.simulate)
 
 
+def setup_parser():
+    parser = argparse.ArgumentParser(description='A tool to automate joining CL-Intro.mp4 to a clip or clips.',
+                                     epilog=f'Written by {__author__}')
+    parser.add_argument('-V', '--version', action='version', version=f"%(prog)s ({__version__})")
+    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Enable verbose output')
+    parser.add_argument('-s', '--summary', action='store_true', dest='summary',
+                        help="Don't print a summary after completion")
+    parser.add_argument('-S', '--simulate', action='store_true', dest='simulate',
+                        help="Simulate calls to ffmpeg (useful for testing or seeing which files would be affected)")
+    parser.add_argument('file_name', help='The [partial] file name of the video[s] you want to edit')
+    return parser.parse_args()
+
+
+def make_command(vid):
+    output_filename = f'"{os.path.splitext(vid)[0] + "_FINAL" + os.path.splitext(vid)[1]}"'
+    command = f'ffmpeg -f concat -safe 0 -i {LISTFILE} -c copy {output_filename}'
+    return output_filename, command
+
+
 def print_summary(result, sim):
-    res_str = f'{"SIMULATED " if sim else ""}Batch operation completed. \
+    res_str = f'{"SIMULATED b" if sim else "B"}atch operation completed. \
 {len(result)} files {"would" if sim else "should"} have been created:'
     separator = '*' * len(res_str)
     print(separator, res_str, separator, sep='\n')
@@ -69,10 +70,14 @@ def verbose_intro(vids):
 
 
 def verbose_list(vid, command):
-    print(f'* contents of {LISTFILE} are now:')
-    print('* file', CL_INTRO)
-    print('* file', f"'{vid}'")
+    print(f'* overwriting {LISTFILE} with the following:')
+    print('** file', CL_INTRO)
+    print('** file', f"'{vid}'")
     print(f'* executing: {command}')
+
+
+def verbose_delete_list():
+    print(f'* deleted {LISTFILE}')
 
 
 if __name__ == '__main__':
