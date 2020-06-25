6c7116a019d74a8ef137e9c515a3a0e47a736198
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-06-24 17:40:36 -0700

    improved verbose output

--- auto_join_CLIntro.py
+++ auto_join_CLIntro.py
@@ -10,7 +10,13 @@ def main():
     result = []
     parser = argparse.ArgumentParser(description='A tool to automate joining CL-Intro.mp4 to a clip or clips.',
                                      epilog='Written by Daniel Karpelevitch (dkarpele)')
-    parser.add_argument('-V', '--version', action='version', version=f"%(prog)s ({__version__})")
+    parser.add_argument('-V', '--version',
+                        action='version',
+                        version=f"%(prog)s ({__version__})")
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        dest='verbose',
+                        help='Enable verbose output')
     parser.add_argument('file_name',
                         help='The [partial] file name of the video[s] you want to edit')
     parser.add_argument('-n', '--no-summary',
@@ -24,17 +30,26 @@ def main():
     args = parser.parse_args()
 
     vids = [file for file in os.listdir(os.curdir) if args.file_name in file]
-
+    if args.verbose:
+        print('* found files:')
+        for vid in vids:
+            print('*', vid)
+        print()
     for vid in vids:
         with open(LISTFILE, mode='w') as f:
             print('file', CL_INTRO, file=f)
             print('file', f"'{vid}'", file=f)
+            if args.verbose:
+                print(f'* contents of {LISTFILE} are now:')
+                print('* file', CL_INTRO)
+                print('* file', f"'{vid}'")
         output = f'"{os.path.splitext(vid)[0] + "_FINAL" + os.path.splitext(vid)[1]}"'
         command = f'ffmpeg -f concat -safe 0 -i {LISTFILE} -c copy {output}'
-        print(f'\n**** {command}')
+        if args.verbose:
+            print(f'* executing: {command}')
         result.append(output)
         if args.simulate:
-            print('* some ffmpeg output *\n'*5)
+            print('- some ffmpeg output -\n' * 5)
         else:
             os.system(command)
 
@@ -47,13 +62,11 @@ def main():
 def print_summary(result, sim):
     res_str = f'{"SIMULATED " if sim else ""}Batch operation completed. \
 {len(result)} files {"would have been " if sim else ""}created:'
-    sep = '*' * len(res_str)
-    print(sep)
-    print(res_str)
-    print(sep)
+    separator = '*' * len(res_str)
+    print(separator, res_str, separator, sep='\n')
     for file in result:
         print(file.strip('"'))
-    print(sep)
+    print(separator)
 
 
 if __name__ == '__main__':
